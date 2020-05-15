1bcbd3a8bbe6fe5cd02c5a817eca852fc7d198f8
SirPepe-roni <lydiadeetz1488@yandex.ru>
2020-05-15 23:22:24 +0300

    Second lab

---
+++ Task2_Кудрявцев Артём_15.05.20.py
@@ -0,0 +1,15 @@
+first=int(input('Введите первое число='))
+second=int(input('Введите второе число='))
+firstAbs = abs(first)
+secondAbs = abs(second)
+sum = 0
+for i in range(firstAbs):
+    sum += secondAbs
+print(sum * (first / firstAbs) * (second / secondAbs))
+
+
+
+
+            
+                
+                
